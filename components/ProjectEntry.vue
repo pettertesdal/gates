e7ada4618fdf257b7e42b6a685f6e05454aeb0e7
installer <installer@gmail.com>
2025-02-28 11:49:11 +0100

    changed how status light works

--- components/ProjectEntry.vue
+++ components/ProjectEntry.vue
@@ -88,11 +88,17 @@ const updateComment = async () => {
 
 // Compute the project status
 const calculateStatus = computed(() => {
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() - 2);
-  const onTimeDate = new Date(props.entryData.onTimeDate);
-  return onTimeDate >= tomorrow;
+  if (fullProgress) {
+    const onTimeDate = new Date(props.entryData.onTimeDate);
+    return onTimeDate < new Date(props.entryData.POdate);
+  }
+  else {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() - 2);
+    const onTimeDate = new Date(props.entryData.onTimeDate);
+    return onTimeDate >= tomorrow;
+  }
 });
 
 
